6a613184b35e21287f3197dfe4a5b26477ad15d7
AlfaPacktor <packtor@mail.ru>
2025-09-01 22:49:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 # ИСПРАВЛЕННАЯ Страница Входа
 def login_page():
     st.header("Добро пожаловать!")
-    username = st.text_input("Введите ваше имя (Например, [jg:person_120])")
+    username = st.text_input("Введите ваше имя (Например, Константинов Ярослав")
 
     if st.button("Войти"):
         if username:
